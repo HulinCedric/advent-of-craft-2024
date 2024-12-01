82efca5ea65c0ca0bb6689b6f69dfc591ae20e08
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-01 19:17:25 +0100

    refactor: Extract Defaults test values

---
+++ exercise/C#/day01/Communication.Tests/Builders/MessageDefaults.cs
@@ -0,0 +1,9 @@
+namespace Communication.Tests.Builders;
+
+public static class MessageDefaults
+{
+    public const string Dasher = "Dasher";
+    public const string NorthPole = "North Pole";
+    public const int NumberOfDaysToRest = 2;
+    public const int NumberOfDayBeforeChristmas = 24;
+}
