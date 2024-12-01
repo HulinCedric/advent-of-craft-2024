bf36a302dfef5c212208b4f673af9d880cac8098
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-01 21:48:47 +0100

    feat: DaysBeforeReturn

---
+++ exercise/C#/day01/Communication/DaysBeforeReturn.cs
@@ -0,0 +1,5 @@
+namespace Communication;
+
+public record DaysBeforeReturn(
+    NumbersOfDaysForComingBack NumbersOfDaysForComingBack,
+    NumberOfDaysBeforeChristmas NumberOfDaysBeforeChristmas);
