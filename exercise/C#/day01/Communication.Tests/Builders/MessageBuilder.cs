e2a34fc64e8f0e7acf79aa7c318cf4c209934262
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-01 19:03:47 +0100

    refactor: Message builder to reduce coupling

---
+++ exercise/C#/day01/Communication.Tests/Builders/MessageBuilder.cs
@@ -0,0 +1,44 @@
+namespace Communication.Tests.Builders;
+
+public class MessageBuilder
+{
+    private string _currentLocation = "North Pole";
+    private int _numberOfDaysBeforeChristmas = 24;
+    private int _numbersOfDaysForComingBack = 2;
+    private string _reindeerName = "Dasher";
+
+    public static MessageBuilder AMessage() => new();
+
+    public MessageBuilder WithReindeerName(string reindeerName)
+    {
+        _reindeerName = reindeerName;
+        return this;
+    }
+
+    public MessageBuilder WithCurrentLocation(string currentLocation)
+    {
+        _currentLocation = currentLocation;
+        return this;
+    }
+
+    public MessageBuilder WithNumbersOfDaysForComingBack(int numbersOfDaysForComingBack)
+    {
+        _numbersOfDaysForComingBack = numbersOfDaysForComingBack;
+        return this;
+    }
+
+    public MessageBuilder WithNumberOfDaysBeforeChristmas(int numberOfDaysBeforeChristmas)
+    {
+        _numberOfDaysBeforeChristmas = numberOfDaysBeforeChristmas;
+        return this;
+    }
+
+    public static implicit operator Message(MessageBuilder builder) => builder.Build();
+
+    public Message Build()
+        => new(
+            new ReindeerName(_reindeerName),
+            new CurrentLocation(_currentLocation),
+            new NumbersOfDaysForComingBack(_numbersOfDaysForComingBack),
+            new NumberOfDaysBeforeChristmas(_numberOfDaysBeforeChristmas));
+}
