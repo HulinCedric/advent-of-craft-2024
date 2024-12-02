8719e23d47ebae69db67196e1da9677108208654
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-02 18:50:35 +0100

    feat: Add "Bang" rule to FizzBuzz tests

--- exercise/C#/day02/Games.Tests/FizzBuzzTests.cs
+++ exercise/C#/day02/Games.Tests/FizzBuzzTests.cs
@@ -9,14 +9,27 @@ namespace Games.Tests;
 public class FizzBuzzTests
 {
     private static readonly string[] FizzBuzzStrings =
-        ["Fizz", "Buzz", "Whizz", "FizzBuzz", "FizzWhizz", "BuzzWhizz", "FizzBuzzWhizz"];
+    [
+        "Fizz", "Buzz", "Whizz", "Bang",
+
+        "FizzBuzz", "FizzWhizz", "FizzBang",
+        "BuzzWhizz", "BuzzBang",
+        "WhizzBang",
+
+        "FizzBuzzWhizz", "FizzBuzzBang",
+        "FizzWhizzBang",
+        "BuzzWhizzBang",
+
+        "FizzBuzzWhizzBang"
+    ];
 
     private readonly FizzBuzz _fizzBuzz = new(
         new Dictionary<int, string>
         {
             { 3, "Fizz" },
             { 5, "Buzz" },
-            { 7, "Whizz" }
+            { 7, "Whizz" },
+            { 11, "Bang" }
         });
 
     [Theory]
@@ -24,14 +37,17 @@ public class FizzBuzzTests
     [InlineData(67, "67")]
     [InlineData(82, "82")]
     [InlineData(3, "Fizz")]
-    [InlineData(66, "Fizz")]
-    [InlineData(99, "Fizz")]
+    [InlineData(6, "Fizz")]
+    [InlineData(9, "Fizz")]
     [InlineData(5, "Buzz")]
     [InlineData(50, "Buzz")]
     [InlineData(85, "Buzz")]
     [InlineData(7, "Whizz")]
     [InlineData(14, "Whizz")]
     [InlineData(28, "Whizz")]
+    [InlineData(11, "Bang")]
+    [InlineData(22, "Bang")]
+    [InlineData(44, "Bang")]
     [InlineData(15, "FizzBuzz")]
     [InlineData(30, "FizzBuzz")]
     [InlineData(45, "FizzBuzz")]
@@ -41,9 +57,20 @@ public class FizzBuzzTests
     [InlineData(35, "BuzzWhizz")]
     [InlineData(70, "BuzzWhizz")]
     [InlineData(140, "BuzzWhizz", Skip = "Over the limit")]
+    [InlineData(33, "FizzBang")]
+    [InlineData(66, "FizzBang")]
+    [InlineData(99, "FizzBang")]
+    [InlineData(55, "BuzzBang")]
+    [InlineData(110, "BuzzBang", Skip = "Over the limit")]
+    [InlineData(77, "WhizzBang")]
+    [InlineData(154, "WhizzBang", Skip = "Over the limit")]
     [InlineData(105, "FizzBuzzWhizz", Skip = "Over the limit")]
     [InlineData(210, "FizzBuzzWhizz", Skip = "Over the limit")]
     [InlineData(420, "FizzBuzzWhizz", Skip = "Over the limit")]
+    [InlineData(115, "FizzBuzzBang", Skip = "Over the limit")]
+    [InlineData(230, "FizzBuzzBang", Skip = "Over the limit")]
+    [InlineData(231, "FizzWhizzBang", Skip = "Over the limit")]
+    [InlineData(1155, "FizzBuzzWhizzBang", Skip = "Over the limit")]
     public void Returns_Number_Representation(int input, string expectedResult)
         => _fizzBuzz.Convert(input)
             .Should()
