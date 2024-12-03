d04683acdfdfb064b86063eccbdfa7846b712517
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-03 22:55:04 +0100

    feat: Invalid toy rules inherit from valid ones

--- exercise/C#/day03/Preparation.Tests/Generators/TooHeavyToyGenerator.cs
+++ exercise/C#/day03/Preparation.Tests/Generators/TooHeavyToyGenerator.cs
@@ -2,7 +2,7 @@ using Bogus;
 
 namespace Preparation.Tests.Generators;
 
-internal class TooHeavyToyGenerator : Faker<Toy>
+internal class TooHeavyToyGenerator : ToyGenerator
 {
     public TooHeavyToyGenerator() => RuleFor(x => x.Weight, f => f.Random.Double(5, double.MaxValue));
 }
