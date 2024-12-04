e66324c81f2c999fe97e5acc41ccddbd0b041539
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-04 22:07:02 +0100

    refactor: file-scoped namespace

--- exercise/C#/day04/Routine.Tests/RoutineTests.cs
+++ exercise/C#/day04/Routine.Tests/RoutineTests.cs
@@ -1,19 +1,18 @@
 using Xunit;
 
-namespace Routine.Tests
-{
-    public class RoutineTests
-    {
-        [Fact]
-        public void StartRoutine_With_FakeItEasy()
-        {
-            // Write a test using FakeItEasy library
-        }
+namespace Routine.Tests;
 
-        [Fact]
-        public void StartRoutine_With_Manual_Test_Doubles()
-        {
-            // Write a test using your own Test Double(s)
-        }
+public class RoutineTests
+{
+    [Fact]
+    public void StartRoutine_With_FakeItEasy()
+    {
+        // Write a test using FakeItEasy library
+    }
+
+    [Fact]
+    public void StartRoutine_With_Manual_Test_Doubles()
+    {
+        // Write a test using your own Test Double(s)
     }
 }
