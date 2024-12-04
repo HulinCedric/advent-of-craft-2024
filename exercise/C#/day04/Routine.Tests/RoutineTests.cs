2575a817f07c2a8ab2c0c71230aafe176c484415
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-04 21:31:45 +0100

    test: StartRoutine_With_FakeItEasy

--- exercise/C#/day04/Routine.Tests/RoutineTests.cs
+++ exercise/C#/day04/Routine.Tests/RoutineTests.cs
@@ -1,3 +1,4 @@
+using FakeItEasy;
 using Xunit;
 
 namespace Routine.Tests;
@@ -7,7 +8,28 @@ public class RoutineTests
     [Fact]
     public void StartRoutine_With_FakeItEasy()
     {
-        // Write a test using FakeItEasy library
+        // Given
+        var emailService = A.Fake<IEmailService>();
+        var scheduleService = A.Fake<IScheduleService>();
+        var reindeerFeeder = A.Fake<IReindeerFeeder>();
+
+        var schedule = new Schedule
+        {
+            Tasks = ["Task 1", "Task 2"]
+        };
+
+        A.CallTo(() => scheduleService.TodaySchedule()).Returns(schedule);
+
+        var routine = new Routine(emailService, scheduleService, reindeerFeeder);
+
+        // When
+        routine.Start();
+
+        // Then
+        A.CallTo(() => scheduleService.OrganizeMyDay(schedule)).MustHaveHappened();
+        A.CallTo(() => reindeerFeeder.FeedReindeers()).MustHaveHappened();
+        A.CallTo(() => emailService.ReadNewEmails()).MustHaveHappened();
+        A.CallTo(() => scheduleService.Continue()).MustHaveHappened();
     }
 
     [Fact]
