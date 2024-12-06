cc6efab54180ff43bb9df8e62f6bb908ef8daa32
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-06 08:57:42 +0100

    feat: make it serial numbe

--- exercise/C#/day05/EID.Tests/EIDShould.cs
+++ exercise/C#/day05/EID.Tests/EIDShould.cs
@@ -39,10 +39,6 @@ public class EIDShould
     private static bool ValidateSex(string input) => input[0] is '1' or '2' or '3';
 
     private static bool ValidateYear(string input) => input[1..3].All(char.IsDigit);
-
-    private static bool ValidateSerialNumber(string input)
-    {
-        var serialNumber = input[3..6];
-        return serialNumber != "xxx";
-    }
+    
+    private static bool ValidateSerialNumber(string input) => input[3..6].All(char.IsDigit);
 }
