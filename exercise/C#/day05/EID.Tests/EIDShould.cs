c113a548a9b8495493f42bb199b12a03ab99fd3e
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-06 08:52:30 +0100

    test: Validate Year
    Change strategy by validating number incrementally for each part

--- exercise/C#/day05/EID.Tests/EIDShould.cs
+++ exercise/C#/day05/EID.Tests/EIDShould.cs
@@ -4,7 +4,6 @@ using Xunit;
 namespace EID.Tests;
 
 // Following Canon TDD here is the list of tests that should be implemented:
-// EID year should be between 00 and 99
 // EID serial number should be between 001 and 999
 // EID control key should be between 01 and 99
 // EID control key should be valid (complement to 97 of the number formed by the first 6 digits of the EID modulo 97)
@@ -14,8 +13,8 @@ public class EIDShould
     [Theory]
     [InlineData("1", "too short")]
     [InlineData("198007671", "too long")]
-    [InlineData("a9800767", "contains letters")]
     [InlineData("49800767", "incorrect sex")]
+    [InlineData("1xx00767", "incorrect year")]
     public void Be_invalid(string input, string reason)
         => Validate(input)
             .Should()
@@ -29,12 +28,12 @@ public class EIDShould
 
     private static bool Validate(string input)
         => ValidateLength(input)
-           && ValidateIsNumber(input)
-           && ValidateSex(input);
+           && ValidateSex(input)
+           && ValidateYear(input);
+
+    private static bool ValidateLength(string input) => input.Length == 8;
 
     private static bool ValidateSex(string input) => input[0] is '1' or '2' or '3';
 
-    private static bool ValidateIsNumber(string input) => input.All(char.IsDigit);
-
-    private static bool ValidateLength(string input) => input.Length == 8;
+    private static bool ValidateYear(string input) => input[1..3].All(char.IsDigit);
 }
