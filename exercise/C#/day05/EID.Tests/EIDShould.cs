4c3f5041729ed52bd76efa1e84d4b20c4aab6ba3
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-06 09:14:19 +0100

    feat: Triangulation full control key

--- exercise/C#/day05/EID.Tests/EIDShould.cs
+++ exercise/C#/day05/EID.Tests/EIDShould.cs
@@ -3,9 +3,6 @@ using Xunit;
 
 namespace EID.Tests;
 
-// Following Canon TDD here is the list of tests that should be implemented:
-// EID control key should be between 01 and 99
-// EID control key should be valid (complement to 97 of the number formed by the first 6 digits of the EID modulo 97)
 // ReSharper disable once InconsistentNaming
 public class EIDShould
 {
@@ -17,6 +14,8 @@ public class EIDShould
     [InlineData("198xxx67", "incorrect serial number")]
     [InlineData("19800067", "incorrect serial number")]
     [InlineData("198007xx", "incorrect control key")]
+    [InlineData("19800799", "incorrect control key")]
+    [InlineData("19800701", "incorrect control key")]
     public void Be_invalid(string input, string reason)
         => Validate(input)
             .Should()
@@ -33,7 +32,7 @@ public class EIDShould
            && ValidateSex(input[0])
            && ValidateYear(input[1..3])
            && ValidateSerialNumber(input[3..6])
-           && ValidateControlKey(input[6..8]);
+           && ValidateControlKey(input[..6], input[6..8]);
 
     private static bool ValidateLength(string input) => input.Length == 8;
 
@@ -44,7 +43,16 @@ public class EIDShould
     private static bool ValidateSerialNumber(string serialNumber)
         => IsANumber(serialNumber) && int.Parse(serialNumber) is >= 1 and <= 999;
 
-    private static bool ValidateControlKey(string controlKey) => IsANumber(controlKey);
+    private static bool ValidateControlKey(string eidWithoutKey, string controlKey)
+    {
+        if (!IsANumber(controlKey)) return false;
+
+        var key = int.Parse(controlKey);
+
+        var checksum = 97 - int.Parse(eidWithoutKey) % 97;
+
+        return key == checksum;
+    }
 
     private static bool IsANumber(string input) => input.All(char.IsDigit);
 }
