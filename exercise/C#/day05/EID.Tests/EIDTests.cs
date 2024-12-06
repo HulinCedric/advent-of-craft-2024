9e96d2de1805d949468a4f3ab7e574c75ce30797
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-06 08:23:45 +0100

    test: Write a list of the test scenarios you want to cover

--- exercise/C#/day05/EID.Tests/EIDTests.cs
+++ exercise/C#/day05/EID.Tests/EIDTests.cs
@@ -3,6 +3,14 @@ using Xunit;
 
 namespace EID.Tests
 {
+    // Following Canon TDD here is the list of tests that should be implemented:
+    // EID should be 8 characters long
+    // EID should contain only digits
+    // EID sex should be 1, 2 or 3
+    // EID year should be between 00 and 99
+    // EID serial number should be between 001 and 999
+    // EID control key should be between 01 and 99
+    // EID control key should be valid (complement to 97 of the number formed by the first 6 digits of the EID modulo 97)
     public class EIDTests
     {
         [Fact]
