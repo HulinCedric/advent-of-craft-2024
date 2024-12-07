f4eb6b945de8c748bfc1547aef3b95077ec811b8
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-07 19:34:26 +0100

    refactor: Given When Then DSL

--- exercise/C#/day07/Workshop.Tests/WorkshopShould.cs
+++ exercise/C#/day07/Workshop.Tests/WorkshopShould.cs
@@ -8,24 +8,34 @@ public class WorkshopShould
 {
     private const string ToyName = "1 Super Nintendo";
 
+    private Gift? _completedGift;
+    private Workshop _workshop = null!;
+
     [Fact]
     public void Mark_a_gift_as_produced_when_elves_finish_making_it()
     {
-        Workshop workshop = AWorkshop().ThatCanProduce(AGift().Called(ToyName));
+        Given(AWorkshop().ThatCanProduce(AGift().Called(ToyName)));
 
-        var completedGift = workshop.CompleteGift(ToyName);
+        When(workshop => workshop.CompleteGift(ToyName));
 
-        Assert.NotNull(completedGift);
-        Assert.Equal(Status.Produced, completedGift.Status);
+        Then(completedGift =>
+            {
+                Assert.NotNull(completedGift);
+                Assert.Equal(Status.Produced, completedGift.Status);
+            });
     }
 
     [Fact]
     public void Return_no_gift_when_elves_never_started_making_it()
     {
-        Workshop workshop = AWorkshop();
+        Given(AWorkshop());
 
-        var completedGift = workshop.CompleteGift("NonExistingToy");
+        When(workshop => workshop.CompleteGift("NonExistingToy"));
 
-        Assert.Null(completedGift);
+        Then(completedGift => Assert.Null(completedGift));
     }
+
+    private void Given(Workshop workshop) => _workshop = workshop;
+    private void Then(Action<Gift?> assert) => assert(_completedGift);
+    private void When(Func<Workshop, Gift?> act) => _completedGift = act(_workshop);
 }
