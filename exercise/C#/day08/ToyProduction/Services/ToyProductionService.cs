38c23eff3c09d989b7fadfaa5de89c5771935128
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-08 12:35:27 +0100

    refactor: Extract AssignToElf method

--- exercise/C#/day08/ToyProduction/Services/ToyProductionService.cs
+++ exercise/C#/day08/ToyProduction/Services/ToyProductionService.cs
@@ -9,13 +9,17 @@ namespace ToyProduction.Services
             var toy = repository.FindByName(toyName);
             if (toy is null) return;
             
+            AssignToElf(toy);
+            
+            repository.Save(toy);
+        }
+
+        private static void AssignToElf(Toy toy)
+        {
             if (toy is {State: State.Unassigned})
             {
                 toy.State = State.InProduction;
             }
-            
-            repository.Save(toy);
-
         }
     }
 }
