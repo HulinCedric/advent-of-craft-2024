d4bc1564824aa611198992facbc10b36c8834cd3
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-11 22:51:24 +0100

    revert: file-scoped namespace

--- exercise/C#/day11/Christmas.Tests/PreparationTests.cs
+++ exercise/C#/day11/Christmas.Tests/PreparationTests.cs
@@ -2,40 +2,41 @@ using FluentAssertions;
 using Xunit;
 using static Christmas.ToyType;
 
-namespace Christmas.Tests;
-
-public class PreparationTests
+namespace Christmas.Tests
 {
-    [Theory]
-    [InlineData(-1, "No gifts to prepare.")]
-    [InlineData(0, "No gifts to prepare.")]
-    [InlineData(1, "Elves will prepare the gifts.")]
-    [InlineData(49, "Elves will prepare the gifts.")]
-    [InlineData(50, "Santa will prepare the gifts.")]
-    public void PrepareGifts(int numberOfGifts, string expected)
-        => Preparation.PrepareGifts(numberOfGifts).Should().Be(expected);
+    public class PreparationTests
+    {
+        [Theory]
+        [InlineData(-1, "No gifts to prepare.")]
+        [InlineData(0, "No gifts to prepare.")]
+        [InlineData(1, "Elves will prepare the gifts.")]
+        [InlineData(49, "Elves will prepare the gifts.")]
+        [InlineData(50, "Santa will prepare the gifts.")]
+        public void PrepareGifts(int numberOfGifts, string expected)
+            => Preparation.PrepareGifts(numberOfGifts).Should().Be(expected);
 
-    [Theory]
-    [InlineData(2, "Baby")]
-    [InlineData(5, "Toddler")]
-    [InlineData(12, "Child")]
-    [InlineData(13, "Teen")]
-    public void CategorizeGift(int age, string expectedCategory)
-        => Preparation.CategorizeGift(age)
-            .Should()
-            .Be(expectedCategory);
+        [Theory]
+        [InlineData(2, "Baby")]
+        [InlineData(5, "Toddler")]
+        [InlineData(12, "Child")]
+        [InlineData(13, "Teen")]
+        public void CategorizeGift(int age, string expectedCategory)
+            => Preparation.CategorizeGift(age)
+                .Should()
+                .Be(expectedCategory);
 
-    [Theory]
-    [InlineData(Educational, 25, 100, true)]
-    [InlineData(Fun, 30, 100, true)]
-    [InlineData(Creative, 20, 100, true)]
-    [InlineData(Educational, 20, 100, false)]
-    [InlineData(Fun, 29, 100, false)]
-    [InlineData(Creative, 15, 100, false)]
-    [InlineData((ToyType)4, 1, 1, false)]
-    [InlineData(Educational, 1, 0, false)]
-    public void EnsureToyBalance(ToyType toyType, int toysCount, int totalToys, bool expected)
-        => Preparation.EnsureToyBalance(toyType, toysCount, totalToys)
-            .Should()
-            .Be(expected);
+        [Theory]
+        [InlineData(Educational, 25, 100, true)]
+        [InlineData(Fun, 30, 100, true)]
+        [InlineData(Creative, 20, 100, true)]
+        [InlineData(Educational, 20, 100, false)]
+        [InlineData(Fun, 29, 100, false)]
+        [InlineData(Creative, 15, 100, false)]
+        [InlineData((ToyType)4, 1, 1, false)]
+        [InlineData(Educational, 1, 0, false)]
+        public void EnsureToyBalance(ToyType toyType, int toysCount, int totalToys, bool expected)
+            => Preparation.EnsureToyBalance(toyType, toysCount, totalToys)
+                .Should()
+                .Be(expected);
+    }
 }
