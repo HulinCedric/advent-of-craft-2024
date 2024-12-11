5363fb3fdd62d3022aa4a66f8596bb3117afc547
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-11 07:20:06 +0100

    refactor: Extract magic strings

--- exercise/C#/day10/Delivery/Delivery.cs
+++ exercise/C#/day10/Delivery/Delivery.cs
@@ -4,26 +4,30 @@ using CalculationStrategy = Func<char, int>;
 
 public static class Building
 {
+    private const char OpeningBracket = '(';
+    private const char ClosingBracket = ')';
+    private const string Elf = "🧝";
+
     public static int WhichFloor(string instructions) => instructions.Sum(DetermineCalculationStrategy(instructions));
 
     private static CalculationStrategy DetermineCalculationStrategy(string instructions)
-        => instructions.Contains("🧝")
+        => instructions.Contains(Elf)
             ? ElfCalculationStrategy()
             : StandardCalculationStrategy();
 
     private static CalculationStrategy StandardCalculationStrategy()
         => instruction => instruction switch
         {
-            '(' => 1,
-            ')' => -1,
+            OpeningBracket => 1,
+            ClosingBracket => -1,
             _ => 0
         };
 
     private static CalculationStrategy ElfCalculationStrategy()
         => instruction => instruction switch
         {
-            '(' => -2,
-            ')' => 3,
+            OpeningBracket => -2,
+            ClosingBracket => 3,
             _ => 0
         };
 }
