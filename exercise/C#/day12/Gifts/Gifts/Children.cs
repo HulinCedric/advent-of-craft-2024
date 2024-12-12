99823db9f1d82089f62df5ac8e198336a7033c9b
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-12 21:17:04 +0100

    refactor: Remove unecessary loop

--- exercise/C#/day12/Gifts/Gifts/Children.cs
+++ exercise/C#/day12/Gifts/Gifts/Children.cs
@@ -2,24 +2,11 @@ namespace Gifts;
 
 public class Children
 {
-    private readonly List<Child> _childrenRepository = [];
+    private readonly List<Child> _children = [];
 
     internal Child FindChildByName(string childName)
-    {
-        Child? found = null;
-        for (int i = 0; i < _childrenRepository.Count; i++)
-        {
-            var currentChild = _childrenRepository[i];
-            if (currentChild.Name == childName)
-            {
-                found = currentChild;
-            }
-        }
+        => _children.FirstOrDefault(child => child.Name == childName)
+           ?? throw new InvalidOperationException("No such child found");
 
-        if (found == null)
-            throw new InvalidOperationException("No such child found");
-        return found;
-    }
-
-    public void AddChild(Child child) => _childrenRepository.Add(child);
+    public void AddChild(Child child) => _children.Add(child);
 }
