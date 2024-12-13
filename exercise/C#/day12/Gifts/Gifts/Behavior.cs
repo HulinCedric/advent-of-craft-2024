0d4955a598324b738e8bcd14e578f3bb09cfbcbb
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-13 09:01:12 +0100

    refactor: Smart enum Behvior

--- exercise/C#/day12/Gifts/Gifts/Behavior.cs
+++ exercise/C#/day12/Gifts/Gifts/Behavior.cs
@@ -4,20 +4,9 @@ namespace Gifts;
 
 public class Behavior(string value)
 {
-    private const string VeryNiceValue = "very nice";
-    private const string NiceValue = "nice";
-    private const string NaughtyValue = "naughty";
-    
-    public static readonly Behavior VeryNice = new(VeryNiceValue);
-    public static readonly Behavior Nice = new(NiceValue);
-    public static readonly Behavior Naughty = new(NaughtyValue);
+    public static readonly Behavior VeryNice = new VeryNiceBehavior();
+    public static readonly Behavior Nice = new NiceBehavior();
+    public static readonly Behavior Naughty = new NaughtyBehavior();
 
-    internal Option<Toy> GetChoice(WishList wishList)
-        => value switch
-        {
-            VeryNiceValue => wishList.GetFirstChoice(),
-            NiceValue => wishList.GetSecondChoice(),
-            NaughtyValue => wishList.GetThirdChoice(),
-            _ => Option<Toy>.None
-        };
+    internal virtual Option<Toy> GetChoice(WishList wishList) => Option<Toy>.None;
 }
