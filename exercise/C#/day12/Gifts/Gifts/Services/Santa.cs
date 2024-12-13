a6594db74124272d86c9cc8206c12c3a268b07ce
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-13 09:07:43 +0100

    refactor: Result alias

--- exercise/C#/day12/Gifts/Gifts/Services/Santa.cs
+++ exercise/C#/day12/Gifts/Gifts/Services/Santa.cs
@@ -4,17 +4,19 @@ using LanguageExt;
 
 namespace Gifts.Services;
 
+using Result = Either<string, Option<Toy>>;
+
 public class Santa(IChildrenRepository childrenRepository)
 {
-    public Either<string, Option<Toy>> ChooseToyForChild(ChildName childName)
+    public Result ChooseToyForChild(ChildName childName)
         => childrenRepository.FindChildByName(childName)
             .Match(
                 ToToyChoice,
                 ToFailure);
 
-    private static Either<string, Option<Toy>> ToToyChoice(Child child) => child.GetChoice();
+    private static Result ToToyChoice(Child child) => child.GetChoice();
 
-    private static Either<string, Option<Toy>> ToFailure() => "No such child found";
+    private static Result ToFailure() => "No such child found";
 
     public void AddChild(Child child) => childrenRepository.AddChild(child);
 }
