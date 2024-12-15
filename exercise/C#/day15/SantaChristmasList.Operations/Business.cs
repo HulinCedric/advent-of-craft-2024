e034144b0454394f9c6f4e517a51610d71314ac5
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-15 14:51:12 +0100

    refactor: Simplification

--- exercise/C#/day15/SantaChristmasList.Operations/Business.cs
+++ exercise/C#/day15/SantaChristmasList.Operations/Business.cs
@@ -14,23 +14,17 @@ public class Business(Factory factory, Inventory inventory, WishList wishList)
             .Bind(LoadGiftFromInventory)
             .Map(LoadToSleigh);
 
-    private static string LoadToSleigh(Gift loadedGift) => $"Gift: {loadedGift.Name} has been loaded!";
-
-    private Either<Error, Gift> LoadGiftFromInventory(ManufacturedGift manufacturedGift)
-        => inventory.PickUpGift(manufacturedGift.BarCode)
-            .Match(
-                Either<Error, Gift>.Right,
-                () => Error.New("Missing gift: The gift has probably been misplaced by the elves!"));
+    private Either<Error, Gift> IdentifyGift(Child child)
+        => wishList.IdentifyGift(child)
+            .ToEither(() => Error.New("Missing gift: Child wasn't nice this year!"));
 
     private Either<Error, ManufacturedGift> FindManufacturedGift(Gift gift)
         => factory.FindManufacturedGift(gift)
-            .Match(
-                Either<Error, ManufacturedGift>.Right,
-                () => Error.New("Missing gift: Gift wasn't manufactured!"));
+            .ToEither(() => Error.New("Missing gift: Gift wasn't manufactured!"));
 
-    private Either<Error, Gift> IdentifyGift(Child child)
-        => wishList.IdentifyGift(child)
-            .Match(
-                Either<Error, Gift>.Right,
-                () => Error.New("Missing gift: Child wasn't nice this year!"));
+    private Either<Error, Gift> LoadGiftFromInventory(ManufacturedGift manufactured)
+        => inventory.PickUpGift(manufactured.BarCode)
+            .ToEither(() => Error.New("Missing gift: The gift has probably been misplaced by the elves!"));
+
+    private static string LoadToSleigh(Gift loadedGift) => $"Gift: {loadedGift.Name} has been loaded!";
 }
