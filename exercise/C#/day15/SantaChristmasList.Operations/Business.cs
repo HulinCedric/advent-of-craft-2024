74224ddb374fe35a15510c691339cd18ed63567e
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-15 13:38:03 +0100

    refactor: FindManufacturedGift either

--- exercise/C#/day15/SantaChristmasList.Operations/Business.cs
+++ exercise/C#/day15/SantaChristmasList.Operations/Business.cs
@@ -17,27 +17,35 @@ public class Business(Factory factory, Inventory inventory, WishList wishList)
     }
 
     private void LoadGiftForChild(Sleigh sleigh, Child child)
+        => IdentifyGift(child)
+            .Match(
+                gift =>
+                {
+                    FindManufacturedGift(gift)
+                        .Match(
+                            manufactured =>
+                            {
+                                var finalGift = inventory.PickUpGift(manufactured.BarCode);
+                                if (finalGift is null)
+                                {
+                                    sleigh.Add(
+                                        child,
+                                        Error.New("Missing gift: The gift has probably been misplaced by the elves!"));
+                                    return;
+                                }
+
+                                sleigh.Add(child, $"Gift: {finalGift.Name} has been loaded!");
+                            },
+                            failure => sleigh.Add(child, failure));
+                },
+                failure => sleigh.Add(child, failure));
+
+    private Either<Error, ManufacturedGift> FindManufacturedGift(Gift gift)
     {
-        IdentifyGift(child).Match(
-            gift =>
-            {
-                var manufactured = factory.FindManufacturedGift(gift);
-                if (manufactured is null)
-                {
-                    sleigh.Add(child, Error.New("Missing gift: Gift wasn't manufactured!"));
-                    return;
-                }
-
-                var finalGift = inventory.PickUpGift(manufactured.BarCode);
-                if (finalGift is null)
-                {
-                    sleigh.Add(child, Error.New("Missing gift: The gift has probably been misplaced by the elves!"));
-                    return;
-                }
-
-                sleigh.Add(child, $"Gift: {finalGift.Name} has been loaded!");
-            },
-            failure => sleigh.Add(child, failure));
+        var manufacturedGift = factory.FindManufacturedGift(gift);
+        return manufacturedGift is not null
+            ? manufacturedGift
+            : Error.New("Missing gift: Gift wasn't manufactured!");
     }
 
     private Either<Error, Gift> IdentifyGift(Child child)
