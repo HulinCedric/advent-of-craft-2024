7ae9ab9a5c09faa7eb337b0b44ec02f887a6b134
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-15 13:11:44 +0100

    refactor: Extract LoadGiftForChild

--- exercise/C#/day15/SantaChristmasList.Operations/Business.cs
+++ exercise/C#/day15/SantaChristmasList.Operations/Business.cs
@@ -9,29 +9,34 @@ public class Business(Factory factory, Inventory inventory, WishList wishList)
         var list = new Sleigh();
         foreach (var child in children)
         {
-            var gift = wishList.IdentifyGift(child);
-            if (gift is null)
-            {
-                list.Add(child, Error.New("Missing gift: Child wasn't nice this year!"));
-                continue;
-            }
-            
-            var manufactured = factory.FindManufacturedGift(gift);
-            if (manufactured is null)
-            {
-                list.Add(child, Error.New("Missing gift: Gift wasn't manufactured!"));
-                continue;
-            }
-
-            var finalGift = inventory.PickUpGift(manufactured.BarCode);
-            if (finalGift is null)
-            {
-                list.Add(child, Error.New("Missing gift: The gift has probably been misplaced by the elves!"));
-                continue;
-            }
-
-            list.Add(child, $"Gift: {finalGift.Name} has been loaded!");
+            LoadGiftForChild(list, child);
         }
         return list;
+    }
+
+    private void LoadGiftForChild(Sleigh sleigh, Child child)
+    {
+        var gift = wishList.IdentifyGift(child);
+        if (gift is null)
+        {
+            sleigh.Add(child, Error.New("Missing gift: Child wasn't nice this year!"));
+            return;
+        }
+            
+        var manufactured = factory.FindManufacturedGift(gift);
+        if (manufactured is null)
+        {
+            sleigh.Add(child, Error.New("Missing gift: Gift wasn't manufactured!"));
+            return;
+        }
+
+        var finalGift = inventory.PickUpGift(manufactured.BarCode);
+        if (finalGift is null)
+        {
+            sleigh.Add(child, Error.New("Missing gift: The gift has probably been misplaced by the elves!"));
+            return;
+        }
+
+        sleigh.Add(child, $"Gift: {finalGift.Name} has been loaded!");
     }
 }
