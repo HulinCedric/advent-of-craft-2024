4bfab89bbb953febcbd697d57a9686d5e9fd8b90
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-15 09:59:09 +0100

    refactor: X5T78Mother

--- exercise/C#/day14/Children.Tests/MappingTests.cs
+++ exercise/C#/day14/Children.Tests/MappingTests.cs
@@ -1,5 +1,5 @@
 using Argon;
-using Children.Db2;
+using static Children.Tests.X5T78Mother;
 
 namespace Children.Tests
 {
@@ -20,58 +20,15 @@ namespace Children.Tests
         [Fact]
         public async Task Map_X5T78_To_Girl()
         {
-            var db2Child = new X5T78
-            {
-                Id = Guid.NewGuid().ToString(),
-                N_1 = "Alice",
-                N_2 = "Marie",
-                N_3 = "Smith",
-                CityOfBirth__pc = "Paradise",
-                Person_BD = "19/03/2017",
-                Salutation = "Girl",
-                Type_pc = "PPMPX_09/1",
-                Serv__Gender__TYPE_pc = "X",
-                DeclaredMonthlySalary__c = "0",
-                LegalDocumentExpirationDate1__c = "01/09/2030",
-                LegalDocumentIssuingCountry1__c = "Paradise",
-                LegalDocumentName1__c = "ID",
-                LegalDocumentNumber1__c = "9892389098",
-                ST_Num = "123",
-                ST____Name = "Sunny Street",
-                ST_C = "Paradise",
-                ST_CID = "99"
-            };
+            var child = _mapper.ToDto(Alice);
 
-            var child = _mapper.ToDto(db2Child);
-            
             await Verify(child, _settings);
         }
 
         [Fact]
         public async Task Map_X5T78_To_Child_For_A_Boy()
         {
-            var db2Child = new X5T78
-            {
-                Id = Guid.NewGuid().ToString(),
-                N_1 = "Bob",
-                N_3 = "Brown",
-                CityOfBirth__pc = "Paradise",
-                Person_BD = "01/09/2021",
-                Salutation = "Boy",
-                Type_pc = "PP0PLX_09/1",
-                Serv__Gender__TYPE_pc = "VJX",
-                DeclaredMonthlySalary__c = "0",
-                LegalDocumentExpirationDate1__c = "12/09/2078",
-                LegalDocumentIssuingCountry1__c = "Paradise",
-                LegalDocumentName1__c = "ID",
-                LegalDocumentNumber1__c = "9U129731873191JK",
-                ST_Num = "9",
-                ST____Name = "Oak Street",
-                ST_C = "Paradise",
-                ST_CID = "98988"
-            };
-
-            var child = _mapper.ToDto(db2Child);
+            var child = _mapper.ToDto(Bob);
 
             await Verify(child, _settings);
         }
