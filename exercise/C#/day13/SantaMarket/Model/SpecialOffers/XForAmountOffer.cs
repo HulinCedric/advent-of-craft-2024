a9dd7466352e51642f8ffb12e29286f2b69aa0b5
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-15 21:24:16 +0100

    refactor: Extract mehtod on XForAmountOffer

--- exercise/C#/day13/SantaMarket/Model/SpecialOffers/XForAmountOffer.cs
+++ exercise/C#/day13/SantaMarket/Model/SpecialOffers/XForAmountOffer.cs
@@ -6,15 +6,21 @@ public class XForAmountOffer(int x, double amount) : IOffer
 {
     public Option<Discount> CalculateDiscount(Product product, double unitPrice, int quantity)
     {
-        Discount? discount = null;
+        if (CanApplyDiscount(quantity)) return Option<Discount>.None;
 
-        if (quantity >= x)
-        {
-            var discountTotal = unitPrice * quantity -
-                                (amount * (quantity / x) + quantity % x * unitPrice);
-            discount = new Discount(product, $"{x} for " + amount, -discountTotal);
-        }
+        var discountTotal = CalculateDiscountTotal(unitPrice, quantity);
 
-        return discount;
+        return new Discount(product, DiscountName(), -discountTotal);
+    }
+
+    private string DiscountName() => $"{x} for " + amount;
+
+    private bool CanApplyDiscount(int quantity) => quantity < x;
+
+    private double CalculateDiscountTotal(double unitPrice, int quantity)
+    {
+        var fullPriceTotal = unitPrice * quantity;
+        var discountedPriceTotal = amount * (quantity / x) + quantity % x * unitPrice;
+        return fullPriceTotal - discountedPriceTotal;
     }
 }
