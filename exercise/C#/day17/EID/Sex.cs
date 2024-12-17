63228bc36bad2c6fd1e9abef9cdb4b421f46f9e5
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-17 13:53:42 +0100

    refactor: Extract class in their files

---
+++ exercise/C#/day17/EID/Sex.cs
@@ -0,0 +1,23 @@
+using LanguageExt;
+
+namespace EID;
+
+internal record Sex
+{
+    private const char SloubiSex = '1';
+    private const char GagnaSex = '2';
+    private const char CatactSex = '3';
+
+    private readonly char _value;
+
+    private Sex(char value) => _value = value;
+
+    public static Either<ParsingError, Sex> Parse(char sexDescription)
+        => sexDescription switch
+        {
+            SloubiSex => new Sex(SloubiSex),
+            GagnaSex => new Sex(GagnaSex),
+            CatactSex => new Sex(CatactSex),
+            _ => new ParsingError("incorrect sex")
+        };
+}
