bee5c7ac800044294765fd9cc31ebc58575cdae0
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-17 13:45:02 +0100

    refactor: Parsing error bind

--- exercise/C#/day17/EID/EID.cs
+++ exercise/C#/day17/EID/EID.cs
@@ -19,7 +19,13 @@ public record EID
 
     public static implicit operator string(EID eid) => eid._value;
 
-    private static bool ValidateLength(string input) => input.Length == ValidLength;
+    private static Either<ParsingError, Unit> ValidateLength(string input)
+        => input.Length switch
+        {
+            < ValidLength => new ParsingError("too short"),
+            > ValidLength => new ParsingError("too long"),
+            _ => Unit.Default
+        };
 
     private static bool ValidateSex(char sex) => sex is SloubiSex or GagnaSex or CatactSex;
 
@@ -40,15 +46,12 @@ public record EID
         => ControlKeyComplement - int.Parse(eidWithoutKey) % ControlKeyComplement;
 
     public static Either<ParsingError, EID> Parse(string input)
+        => ValidateLength(input)
+            .Bind(_ => ContinueParse(input));
+
+    private static Either<ParsingError, EID> ContinueParse(string input)
     {
-        if (input.Length < ValidLength)
-            return new ParsingError("too short");
-        
-        if (input.Length > ValidLength)
-            return new ParsingError("too long");
-        
-        if (!(ValidateLength(input)
-              && ValidateSex(input[0])
+        if (!(ValidateSex(input[0])
               && ValidateYear(input[1..3])
               && ValidateSerialNumber(input[3..6])
               && ValidateControlKey(input[..6], input[6..8])))
