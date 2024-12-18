af074cd6295888617a971644c738b40caf8225b4
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-18 01:01:52 +0100

    refactor: EIDGenerator

--- exercise/C#/day17/EID.Tests/EIDGenerator.cs
+++ exercise/C#/day17/EID.Tests/EIDGenerator.cs
@@ -5,15 +5,30 @@ namespace EID.Tests;
 public static class EIDGenerator
 {
     public static Arbitrary<EID> Generate()
-        => (from sexValue in Gen.Choose(1, 3)
-                let sex = Sex.Parse(sexValue)
-                    .Match(s => s, _ => throw new InvalidOperationException("Invalid Sex value"))
-                from yearValue in Gen.Choose(0, 99)
-                let year = Year.Parse(yearValue)
-                    .Match(y => y, _ => throw new InvalidOperationException("Invalid Year value"))
-                from serialNumberValue in Gen.Choose(1, 999)
-                let serialNumber = SerialNumber.Parse(serialNumberValue)
-                    .Match(sn => sn, _ => throw new InvalidOperationException("Invalid Serial Number value"))
-                select new EID(sex, year, serialNumber)
-            ).ToArbitrary();
+        => (from sex in GenerateSex()
+                from year in GenerateYear()
+                from serialNumber in GenerateSerialNumber()
+                select new EID(sex, year, serialNumber))
+            .ToArbitrary();
+
+    private static Gen<Sex> GenerateSex()
+        => from value in Gen.Choose(1, 3)
+            select Sex.Parse(value)
+                .Match(
+                    s => s,
+                    error => throw new InvalidOperationException(error.Reason));
+
+    private static Gen<Year> GenerateYear()
+        => from value in Gen.Choose(0, 99)
+            select Year.Parse(value)
+                .Match(
+                    y => y,
+                    error => throw new InvalidOperationException(error.Reason));
+
+    private static Gen<SerialNumber> GenerateSerialNumber()
+        => from value in Gen.Choose(1, 999)
+            select SerialNumber.Parse(value)
+                .Match(
+                    sn => sn,
+                    error => throw new InvalidOperationException(error.Reason));
 }
