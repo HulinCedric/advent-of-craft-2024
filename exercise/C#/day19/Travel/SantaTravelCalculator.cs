0769098233f558402b8e760df69c718ffd28bfa8
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-19 14:28:57 +0100

    refactor: Bitwise operations

--- exercise/C#/day19/Travel/SantaTravelCalculator.cs
+++ exercise/C#/day19/Travel/SantaTravelCalculator.cs
@@ -1,15 +1,6 @@
-﻿namespace Travel
-{
+﻿namespace Travel;
+
 public static class SantaTravelCalculator
 {
-    public static ulong CalculateTotalDistance(int numberOfReindeers)
-    {
-        ulong distance = 0;
-        for (int i = 0; i < numberOfReindeers; i++)
-        {
-            distance = checked(2 * distance + 1);
-        }
-        return distance;
-    }
-}
+    public static ulong CalculateTotalDistance(int numberOfReindeers) => (1UL << numberOfReindeers) - 1;
 }
