05dccd9a1ad223300e36f7a77694e467497fc59c
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-20 18:11:00 +0100

    test: Add contract test on response with Verify.Http

--- exercise/C#/day20/Reindeer.Web.Tests/ReindeerContractTests.cs
+++ exercise/C#/day20/Reindeer.Web.Tests/ReindeerContractTests.cs
@@ -1,6 +1,4 @@
-using System.Net;
 using System.Net.Http.Json;
-using FluentAssertions;
 using Reindeer.Web.Service;
 
 namespace Reindeer.Web.Tests
@@ -19,25 +17,26 @@ namespace Reindeer.Web.Tests
         public async Task ShouldGetReindeer()
         {
             var response = await _client.GetAsync("reindeer/40F9D24D-D3E0-4596-ADC5-B4936FF84B19");
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            await Verify(response);
         }
-        
+
         [Fact]
         public async Task NotFoundForNotExistingReindeer()
         {
             var nonExistingReindeer = Guid.NewGuid().ToString();
             var response = await _client.GetAsync($"reindeer/{nonExistingReindeer}");
 
-            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            await Verify(response);
         }
-        
+
         [Fact]
         public async Task ConflictWhenTryingToCreateExistingOne()
         {
             var request = new ReindeerToCreateRequest("Petar", ReindeerColor.Purple);
             var response = await _client.PostAsync("reindeer", JsonContent.Create(request));
 
-            response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            await Verify(response);
         }
     }
 }
---
+++ exercise/C#/day20/Reindeer.Web.Tests/ReindeerModuleInitializer.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Reindeer.Web.Tests;
+
+public static class ReindeerModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize() => VerifyHttp.Initialize();
+}
