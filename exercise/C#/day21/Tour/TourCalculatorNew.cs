f4c133975efd23953b23e80f247aebb417248478
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-21 15:29:29 +0100

    refactor: remove redundant `calculated` flag in TourCalculator

--- exercise/C#/day21/Tour/TourCalculatorNew.cs
+++ exercise/C#/day21/Tour/TourCalculatorNew.cs
@@ -7,9 +7,7 @@ namespace Tour;
 public class TourCalculatorNew(List<Step> steps)
 {
     private readonly Seq<Step> _steps = steps.ToSeq();
-
-    private bool calculated = false;
-
+    
     public Either<string, string> Calculate()
     {
         if (_steps.IsNull() || _steps.Count == 0)
@@ -23,17 +21,13 @@ public class TourCalculatorNew(List<Step> steps)
 
         foreach (var s in _steps.OrderBy(x => x.Time))
         {
-            if (!calculated)
-            {
-                deliveryTime += s.DeliveryTime;
-                result.AppendLine(s.ToString());
-            }
+            deliveryTime += s.DeliveryTime;
+            result.AppendLine(s.ToString());
         }
 
         string hhMmSs = @"hh\:mm\:ss";
         string str = TimeSpan.FromSeconds(deliveryTime).ToString(hhMmSs);
         result.AppendLine($"Delivery time | {str}");
-        calculated = true;
 
         return Right(result.ToString());
     }
