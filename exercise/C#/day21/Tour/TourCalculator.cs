030af107d0c4ef52fabc64c3d4d8f29709f8718d
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-21 14:31:46 +0100

    refactor: move Step record to its own file

--- exercise/C#/day21/Tour/TourCalculator.cs
+++ exercise/C#/day21/Tour/TourCalculator.cs
@@ -4,11 +4,6 @@ using static LanguageExt.Prelude;
 
 namespace Tour
 {
-    public record Step(TimeOnly Time, string Label, int DeliveryTime)
-    {
-        public override string ToString() => $"{Time} : {Label} | {DeliveryTime} sec";
-    }
-
     public class TourCalculator
     {
         public List<Step> Steps { get; set; }
