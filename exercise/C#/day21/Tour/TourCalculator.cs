f382c0f18f0c0bc686d59d02763ac486bb92ef06
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-21 14:31:36 +0100

    refactor: simplify ToString implementation in Step record

--- exercise/C#/day21/Tour/TourCalculator.cs
+++ exercise/C#/day21/Tour/TourCalculator.cs
@@ -6,9 +6,7 @@ namespace Tour
 {
     public record Step(TimeOnly Time, string Label, int DeliveryTime)
     {
-        public override string ToString() => fLine();
-
-        public string fLine() => $"{this.Time} : {this.Label} | {this.DeliveryTime} sec";
+        public override string ToString() => $"{Time} : {Label} | {DeliveryTime} sec";
     }
 
     public class TourCalculator
