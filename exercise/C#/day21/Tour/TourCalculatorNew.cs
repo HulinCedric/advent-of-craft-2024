fd401929572d28af51cf492a2422eb78cff758dd
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-21 15:43:18 +0100

    refactor: simplify tour details aggregation logic

--- exercise/C#/day21/Tour/TourCalculatorNew.cs
+++ exercise/C#/day21/Tour/TourCalculatorNew.cs
@@ -7,26 +7,25 @@ namespace Tour;
 public class TourCalculatorNew(List<Step> steps)
 {
     private readonly Seq<Step> _steps = steps.ToSeq();
-    
+
     public Either<string, string> Calculate()
     {
         if (_steps.IsNull() || _steps.Count == 0)
         {
             return Left("No locations !!!");
         }
-
-
+        
         var result = new StringBuilder();
+
+        var tourStepDetails = _steps.OrderBy(x => x.Time)
+            .Aggregate(string.Empty, (acc, s) => acc + s + Environment.NewLine);
         
-        foreach (var s in _steps.OrderBy(x => x.Time))
-        {
-            result.AppendLine(s.ToString());
-        }
-        
-        var tourDeliveryTime = _steps.Select(s=>s.DeliveryTime).Sum();
+        var tourDeliveryTime = _steps.Select(s => s.DeliveryTime).Sum();
 
         string hhMmSs = @"hh\:mm\:ss";
         string str = TimeSpan.FromSeconds(tourDeliveryTime).ToString(hhMmSs);
+       
+        result.Append(tourStepDetails);
         result.AppendLine($"Delivery time | {str}");
 
         return Right(result.ToString());
