0a19b84ffaca3d18010f02b8bf7f8583f071401a
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-22 14:56:27 +0100

    fix: refactor ToInt method using LanguageExt parseInt
    Not escape regex value

--- exercise/C#/day22/EID/StringExtensions.cs
+++ exercise/C#/day22/EID/StringExtensions.cs
@@ -1,19 +1,10 @@
-using System.Text.RegularExpressions;
 using LanguageExt;
-using static LanguageExt.Option<int>;
+using static LanguageExt.Prelude;
 
 namespace EID
 {
-    public static partial class StringExtensions
+    public static class StringExtensions
     {
-        public static Option<int> ToInt(this string potentialNumber)
-            => IsANumber(potentialNumber)
-                ? int.Parse(potentialNumber)
-                : None;
-
-        private static bool IsANumber(string str) => NumberRegex().Match(str).Success;
-
-        [GeneratedRegex("[0-9.]+")]
-        private static partial Regex NumberRegex();
+        public static Option<int> ToInt(this string potentialNumber) => parseInt(potentialNumber);
     }
 }
