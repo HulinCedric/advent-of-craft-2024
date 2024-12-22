2a07b90bf66e4ce460c44b87f4ce3a15bfc0b084
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-22 13:49:36 +0100

    refactor: SexMutator with invalid char

--- exercise/C#/day22/EID.Tests/Generators/Mutators/SexMutator.cs
+++ exercise/C#/day22/EID.Tests/Generators/Mutators/SexMutator.cs
@@ -4,8 +4,22 @@ namespace EID.Tests.Generators.Mutators;
 
 internal record SexMutator() : EIDMutator(
     "Sex mutator",
-    eid => Gen.Choose(4, 9)
-        .Select(invalidSex => invalidSex + eid.ToString()[1..8]))
+    GenerateInvalidSex)
 {
+    private static Gen<string> GenerateInvalidSex(EID eid)
+        => Gen.OneOf(
+            GenerateInvalidSexNumber(eid),
+            GenerateInvalidSexChar(eid));
+
+    private static Gen<string> GenerateInvalidSexChar(EID eid)
+        => from invalidSexNumber in Arb.Default.Char()
+                .Generator
+                .Where(c => c != '1' && c != '2' && c != '3')
+            select $"{invalidSexNumber}{eid.ToString()[1..8]}";
+
+    private static Gen<string> GenerateInvalidSexNumber(EID eid)
+        => from invalidSexNumber in Gen.Choose(4, 9)
+            select $"{invalidSexNumber}{eid.ToString()[1..8]}";
+
     internal static EIDMutator Create() => new SexMutator();
 }
