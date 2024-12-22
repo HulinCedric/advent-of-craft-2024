e91d58ebd21272d0d79d4412d8599ab36bcfa626
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-22 15:06:02 +0100

    refactor: enhance invalid serial number generation logic

--- exercise/C#/day22/EID.Tests/Generators/Mutators/SerialNumberMutator.cs
+++ exercise/C#/day22/EID.Tests/Generators/Mutators/SerialNumberMutator.cs
@@ -7,13 +7,20 @@ internal record SerialNumberMutator() : EIDMutator(
     MutateWithInvalidSerialNumber)
 {
     private static Gen<string> MutateWithInvalidSerialNumber(EID eid)
-        => from invalidSerialNumber in GenerateInvalidSerialNumberString()
+        => from invalidSerialNumber in GenerateInvalidSerialNumber()
             select $"{eid.ToString()[..3]}{invalidSerialNumber}{eid.ToString()[6..8]}";
+
+    private static Gen<string> GenerateInvalidSerialNumber()
+        => Gen.OneOf(
+            GenerateInvalidSerialNumberString(),
+            GenerateInvalidSerialNumberValue());
 
     private static Gen<string> GenerateInvalidSerialNumberString()
         => Arb.Default.String()
             .Generator
             .Where(s => s is { Length: 3 } && !int.TryParse(s, out _));
+
+    private static Gen<string> GenerateInvalidSerialNumberValue() => Gen.Elements(0).Select(s => s.ToString("d3"));
 
     internal static EIDMutator Create() => new SerialNumberMutator();
 }
