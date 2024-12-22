d84e7e07d5db7cfe1817e124a499351057442fb4
Cédric Hulin <hulin.cedric@gmail.com>
2024-12-22 12:43:19 +0100

    refactor: streamline invalid sex generation logic

--- exercise/C#/day22/EID.Tests/Generators/Mutators/SexMutator.cs
+++ exercise/C#/day22/EID.Tests/Generators/Mutators/SexMutator.cs
@@ -4,11 +4,8 @@ namespace EID.Tests.Generators.Mutators;
 
 internal record SexMutator() : EIDMutator(
     "Sex mutator",
-    eid =>
-        Arb.Default.PositiveInt()
-            .Filter(i => i.Item > 3)
-            .Generator
-            .Select(invalidSex => invalidSex + eid.ToString()[1..8]))
+    eid => Gen.Choose(4, 9)
+        .Select(invalidSex => invalidSex + eid.ToString()[1..8]))
 {
     internal static EIDMutator Create() => new SexMutator();
 }
